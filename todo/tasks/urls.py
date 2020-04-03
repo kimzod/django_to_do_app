6bcc705b0b824140f1dad53e0945b2c1343fd264
kimzod <51065991+kimzod@users.noreply.github.com>
2020-04-03 17:28:01 +0900

    complete commit

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('update/<str:pk>/', views.update, name='update'),
+    path('delete/<str:pk>/', views.delete, name='delete'),
 ]
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,5 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
+from .form import TodoForm
 
 
 def index(request):
-    return render(request, 'tasks/index.html')
+    todo_list = Todo.objects.order_by('-created')
+
+    form = TodoForm()
+
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+
+    context = {'todo_list': todo_list, 'form': form}
+    return render(request, 'tasks/index.html', context)
+
+
+def update(request, pk):
+
+    task = Todo.objects.get(id=pk)
+
+    # 작성한Todo 글을 가져온다.
+    form = TodoForm(instance=task)
+
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    context = {'form': form}
+
+    return render(request, 'tasks/update.html', context)
+
+
+def delete(request, pk):
+    item = Todo.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect("/")
+
+    context = {'item': item}
+    return render(request, 'tasks/delete.html', context)
